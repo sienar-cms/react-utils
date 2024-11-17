f77b127a10ad1f376bcaed16affc94f3767a37aa
Christian LeVesque <christian@levesque.dev>
2024-11-16 18:19:47 -0600

    removed external replacements so the built lib has bare imports

--- src/externals.ts
+++ src/externals.ts
@@ -5,11 +5,3 @@ export const viteExternals = [
 	'react-router',
 	'react-router-dom'
 ];
-
-export const viteOutputReplacementPaths = {
-	'react': 'https://esm.sh/react@18.3.1',
-	'react/jsx-runtime': 'https://esm.sh/react@18.3.1/jsx-runtime',
-	'react-dom': 'https://esm.sh/react-dom@18.3.1',
-	'react-router': 'https://esm.sh/react-router@6.26.2',
-	'react-router-dom': 'https://esm.sh/react-router-dom@6.26.2'
-}
